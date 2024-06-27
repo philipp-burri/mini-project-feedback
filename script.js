3ce256a995dacf6bdb4a23ede508afa4c27cd219
philipp-burri <burri.philipp13@gmail.com>
2024-06-27 16:09:14 +0200

    added onclick functions with text display

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+function handleGoodFeedback() {
+    document.getElementById('feedback').style.color = 'green';
+    document.getElementById('emojiSmile').style.fill = 'green';
+    document.getElementById('feedback').innerHTML = 'Vielen Dank, das freut mich sehr!';
+}
+
+function handleBadFeedback() {
+    document.getElementById('feedback').style.color = 'red';
+    document.getElementById('emojiSmile').style.fill = 'red';
+    document.getElementById('feedback').innerHTML = 'Oh nein, das ist aber schade!';
+}
+
 let feedback = document.getElementById("feedback").innerHTML;
 let smiley = document.getElementById("emojiSmile").innerHTML;
-let smileyGood = document.getElementById("GoodSmiley").innerHTML;
-
